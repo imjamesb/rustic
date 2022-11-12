e184c8eff5d1b7d8cb87700d237c3f5caa9d43cd
James Bradlee <post@jamesb.no>
2022-11-12 05:33:12 +0100

    Removed error from mod.ts

--- mod.ts
+++ mod.ts
@@ -1,7 +1,6 @@
 // Copyright 2022 James Bradlee. All rights reserved. MIT License.
 
 export type { Display, FmtResult } from "./Display.ts";
-export type { Rerror } from "./Error.ts";
 export { format, write } from "./ops.ts";
 export { None, Option, Some } from "./Option.ts";
 export { panic } from "./panic.ts";
