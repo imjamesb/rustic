b69989f853da850da8f8a37c1a15c026f04b3476
James Bradlee <post@jamesb.no>
2022-11-12 04:11:06 +0100

    Add documentation to panic.ts

--- panic.ts
+++ panic.ts
@@ -1,9 +1,40 @@
 import { sprintf } from "./_/printf.ts";
 
-// deno-lint-ignore no-explicit-any
-export function panic(value: any, ...args: string[]) {
+/**
+ * Throw any value.
+ * @param value The value to throw.
+ * @example Panic with an unknown value.
+ *
+ * ```ts
+ * import { panic } from "./mod.ts";
+ * panic(123);
+ * ```
+ *
+ * @example Panic with an error.
+ *
+ * ```ts
+ * import { panic } from "./mod.ts";
+ * panic(new Error("hello world"));
+ * ```
+ */
+export function panic(value: unknown): never;
+/**
+ * Throw a formatted error.
+ * @param formatter A string that indicates how the error should be formatted.
+ * @param args The arguments to pass to the formatter.
+ * @example
+ * Panic with a string message. This will format the string and turn it into an
+ * Error object.
+ *
+ * ```ts
+ * import { panic } from "./mod.ts";
+ * panic("hello %s", "world");
+ * ```
+ */
+export function panic(formatter: string, ...args: unknown[]): never;
+export function panic(value: unknown, ...args: unknown[]): never {
   if (typeof value === "string") {
-    throw sprintf(value, ...args);
+    throw new Error(sprintf(value, ...args));
   }
   throw value;
 }
