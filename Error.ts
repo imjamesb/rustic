00b3ce33b04bc93aa051c1266878d0bd9ce740c2
James Bradlee <post@jamesb.no>
2022-11-12 05:31:55 +0100

    Removed rustic error

--- Error.ts
+++
@@ -1,7 +0,0 @@
-import type { Option } from "./Option.ts";
-import type { Display } from "./Display.ts";
-
-export interface Rerror extends Display {
-  source(): Option<Rerror>;
-  stack(): Option<string>;
-}
